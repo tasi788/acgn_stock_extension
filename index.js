fe166b78cff7284b57a8d72f170351ba36c64c62
tasi788 <tasi788@gmail.com>
2017-10-24 21:42:31 +0800

    upload

---
+++ index.js
@@ -0,0 +1,11 @@
+// ==UserScript==
+// @name         aniti-acgn-ad
+// @namespace    https://raw.githubusercontent.com/tasi788/acgn_stock_extension/master/index.js
+// @version      0.1
+// @description  take the ad away~~
+// @author       tasi788
+// @match        https://acgn-stock.com/
+// @grant        none
+// ==/UserScript==
+
+$(".fixed-bottom").hide();
